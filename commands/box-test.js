71ec561c31740159b29dae818bd001217f510625
Joshua Thompson <sabrstryk@gmail.com>
2020-09-21 11:44:21 -0400

    Added random selection to box-test command

--- commands/box-test.js
+++ commands/box-test.js
@@ -1,9 +1,17 @@
+const sticks = require('../resources/very_easy.json')
+// const regSticks = sticks.filter(n => !n.special)
+
 // Testing command for box of doom.
 module.exports = {
   name: 'box-test',
   description: 'Test command for box of doom.  Currently only returns a test message.',
   execute (message, args) {
-    message.channel.send('This is only a test stick.')
-    message.channel.send('You have 15 minutes to write 200 words.')
+    const draw = sticks[Math.floor(Math.random() * sticks.length)]
+    message.channel.send('This is a test stick, from the very easy set.')
+    if (draw.special) {
+      message.channel.send(`You have drawn ${draw.name}: ${draw.description}.`)
+    } else {
+      message.channel.send(`You have drawn ${draw.name} words.`)
+    }
   }
 }
