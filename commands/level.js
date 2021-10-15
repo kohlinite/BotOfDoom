895fa81339e771c8f628b7ab1f8d35bbd2ab3b43
Joshua Thompson <sabrstryk@gmail.com>
2021-10-15 16:52:28 -0400

    Cleaned up for 'all' difficulty

--- commands/level.js
+++ commands/level.js
@@ -39,14 +39,13 @@ module.exports = {
     // Find and return requested range
     const requestedSticks = sticks[args[0]]
     const values = requestedSticks.map(a => Number.parseInt(a.name))
-    console.log(values)
     const filteredValues = values.filter(n => !Number.isNaN(n))
-    console.log(filteredValues)
     const min = filteredValues.reduce((a, b) => Math.min(a, b))
     const max = filteredValues.reduce((a, b) => Math.max(a, b))
-    const reply = `Difficulty ${requestedSticks[0].difficulty} has a range from ${min} to ${max}.`
-    console.log('Range requested')
-    console.log(reply)
-    message.channel.send(reply)
+    if (args[0] === 'all') {
+      message.channel.send(`Difficulty All has a range from ${min} to ${max}.`)
+    } else {
+      message.channel.send(`Difficulty ${requestedSticks[0].difficulty} has a range from ${min} to ${max}.`)
+    }
   }
 }
