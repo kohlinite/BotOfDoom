475c53fcbeacb5f06efb42954a071fed681157ca
Joshua Thompson <sabrstryk@gmail.com>
2021-10-15 16:17:49 -0400

    NaN fix

--- commands/level.js
+++ commands/level.js
@@ -38,7 +38,7 @@ module.exports = {
     }
     // Find and return requested range
     const requestedSticks = sticks[args[0]]
-    const values = requestedSticks.map(a => Number.parseInt(a))
+    const values = requestedSticks.map(a => Number.parseInt(a)).filter(n => !Number.isNaN(n))
     const min = values.reduce((a, b) => Math.min(a, b))
     const max = values.reduce((a, b) => Math.max(a, b))
     const reply = `Difficulty ${requestedSticks.difficulty} has a range from ${min} to ${max}.`
