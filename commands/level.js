887d2b3665c241c678f182ac6c0db58b751a28ec
Joshua Thompson <sabrstryk@gmail.com>
2021-10-15 15:30:10 -0400

    Adding range command.

---
+++ commands/level.js
@@ -0,0 +1,46 @@
+/*
+* Command for retrieving word count range.
+*/
+
+// Set up a persistent object containing the various sticks.
+const sticks = {}
+sticks.all = require('../resources/sticks.json')
+// Create additional arrays by difficulty
+sticks.ve = sticks.all.filter(n => n.difficulty === 'Very Easy').filter(n => !n.special)
+sticks.e = sticks.all.filter(n => n.difficulty === 'Easy').filter(n => !n.special)
+sticks.m = sticks.all.filter(n => n.difficulty === 'Medium').filter(n => !n.special)
+sticks.h = sticks.all.filter(n => n.difficulty === 'Hard').filter(n => !n.special)
+sticks.vh = sticks.all.filter(n => n.difficulty === 'Very Hard').filter(n => !n.special)
+
+// Command properties
+module.exports = {
+  name: 'level',
+  aliases: 'range',
+  description: 'Returns the range of word counts for a provided difficulty.',
+  usage: '<difficulty: `ve`|`e`|`m`|`h`|`vh`|`all`>',
+  execute (message, args) {
+    if (!args[0]) {
+      message.channel.send('I need a difficulty selection!')
+      return
+    }
+    // Check arg validity
+    const validArgs = [
+      've',
+      'e',
+      'm',
+      'h',
+      'vh',
+      'all'
+    ]
+    if (!validArgs.some(e => e === args[0])) {
+      message.channel.send('Invalid difficulty specified.  Please specify one of {`ve`,`e`,`m`,`h`,`vh`,`all`}.')
+      return
+    }
+    // Find and return requested range
+    const requestedSticks = sticks[args[0]]
+    const reply = `Difficulty ${requestedSticks.difficulty} has a range from ${requestedSticks[0].name} to ${requestedSticks[requestedSticks.length - 1]}.`
+    console.log('Range requested')
+    console.log(reply)
+    message.channel.send(reply)
+  }
+}
